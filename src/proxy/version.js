80fd5ad368052412bc97c2a9d6521d2be3c2661e
Alexandre Morgaut <alexandre.morgaut@wiztivi.com>
2019-01-04 00:45:11 +0100

    Update version.js

--- src/proxy/version.js
+++ src/proxy/version.js
@@ -14,18 +14,23 @@
 import os from 'os';
 import httpFetch from 'node-fetch';
 
-import { version } from '../package.json';
+import { version } from '../../package.json';
 import { BROWSER, USER_AGENT, WEBKIT_VERSION } from '../config';
+import { proxyDebuggerUrl } from "./list";
 
 export async function getVersion(target) {
     const response = await httpFetch(`${target}/json/version`);
-    const json = await response.json();
 
-    json.Browser = BROWSER;
-    json['Protocol-Version'] = '1.3';
-    json['V8-Version'] = process.versions.v8;
-    json['User-Agent'] = `Mozilla/5.0 (${os.type()}; ${os.arch()} ${os.type()} ${os.release()}) DevTools-Plus/${version} ${USER_AGENT}`;
-    json['WebKit-Version'] = WEBKIT_VERSION;
+    const ua = `Mozilla/5.0 (${os.type()}; ${os.arch()} ${os.type()} ${os.release()}) DevTools-Plus/${version} ${USER_AGENT}`;
+    // const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3601.0 Safari/537.36';
 
-    return json;
+    return Object.assign(await response.json(), {
+        Browser: BROWSER,
+        'Protocol-Version': '1.2',
+        'V8-Version': process.versions.v8,
+        'User-Agent': ua,
+        'WebKit-Version': WEBKIT_VERSION,
+        webSocketDebuggerUrl: await proxyDebuggerUrl,
+
+    });
 }
