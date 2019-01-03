a9caed27a58b030908f2fa7cdf3d3535377ba53c
Alexandre Morgaut <alexandre.morgaut@wiztivi.com>
2019-01-04 00:43:30 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,8 +7,8 @@
 
 export const PORT = 8229;
 export const TARGET = `http://127.0.0.1:${process.debugPort}`;
-export const FAVICON = '../favicon.ico'; // todo handle local favicon
-export const BROWSER = 'DevToolsPlus';
+export const FAVICON = '../favicon.ico';
+export const BROWSER = `NodePlus/70.0.NodeJS.${process.version}`;
 export const USER_AGENT = '';
-export const WEBKIT_VERSION = '';
+export const WEBKIT_VERSION = '537.36 (@cfede9db1d154de0468cb0538479f34c0755a0f4)';
 export const DESCRIPTION = 'with DevTools-Plus';
